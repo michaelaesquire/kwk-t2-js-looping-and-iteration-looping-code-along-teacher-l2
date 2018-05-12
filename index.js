035bbedb1d1e3385556ae6612379fbb0e4e1e650
Michaela Olson <mco44@cornell.edu>
2018-05-12 17:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+function printBadges(nameList) {
+  for (let age = 0; age < 3; age++) {
+    let idNum = age+1;
+  console.log("Welcome " + nameList[age] +"! You are employee #" + idNum + ".");
+  }
+  return nameList;
+}
+
+//function tailsNeverFails() {
+
+
+//  return Math.random() >= 0.5;
+//}
+
+/* function tailsNeverFails() {
+  return Math.random() >= 0.5;
+  let counter = 0;
+}
+
+while (tailsNeverFails()) {
+  console.log("You got " + counter + " tails in a row!");
+    counter++;
+} */
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(Math.random() >= 0.5) {
+      counter++;
+  }
+  let returnMessage = "You got " + counter + " tails in a row!";
+  return returnMessage;
+  console.log("You got " + counter + " tails in a row!");
+}
